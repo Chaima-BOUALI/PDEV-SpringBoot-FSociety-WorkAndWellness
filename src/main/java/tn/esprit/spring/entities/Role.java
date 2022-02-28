3df6d21fa536b444eda4a9c64dc0c304d16a88c3
khaledsaidi <74121742+khaledsaidi@users.noreply.github.com>
2022-02-28 19:22:25 +0100

    entities

---
+++ src/main/java/tn/esprit/spring/entities/Role.java
@@ -0,0 +1,60 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EnableScheduling
+/**
+ * The type Role. Defines the role and the list of users who are associated with that role
+ */
+@Entity(name = "ROLE")
+@Table(name = "ROLES_TABLE")
+public class Role implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ROLE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRole;
+
+    @Column(name = "ROLE_NAME")
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    private RoleName role;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<User> userList = new HashSet<>();
+}
