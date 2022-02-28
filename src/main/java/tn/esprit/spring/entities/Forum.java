ca872d8d55c02484219143d1f2c59b63c6e73023
ChaymaBoualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-02-28 15:37:55 +0100

    Entity Creation

---
+++ src/main/java/tn/esprit/spring/entities/Forum.java
@@ -0,0 +1,73 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "FORUM")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EnableScheduling
+@Table(name = "FORUM_TABLE")
+public class Forum implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idForum;
+	@Column(length = 3000)
+	private String forumName;
+	@Column(length = 3000)
+	private String forumDescription;
+	private Date dateEvent;
+	private int idDocument;
+	@Column(length = 3000)
+	private String documentName;
+	@Column(length = 3000)
+	private String documentType;
+	@Column(length = 3000)
+	@Lob
+	private byte[] dataDocument;
+	private int idImage;
+	@Column(length = 3000)
+	private String imageName;
+	@Column(length = 3000)
+	private String imageType;
+	@Column(length = 3000)
+	private String imageURL;
+	@Lob
+	private byte[] dataImage;
+	@OneToMany(mappedBy = "forum")
+	private Set<Subjects> subject;
+	@ManyToMany
+	@JoinTable(name = "forum_user", joinColumns = { @JoinColumn(name = "idforum") }, inverseJoinColumns = {
+			@JoinColumn(name = "idUser") })
+	Set<User> users;
+
+}
