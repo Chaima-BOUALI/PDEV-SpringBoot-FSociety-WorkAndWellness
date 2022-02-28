53060ceab76db1e93c17ab86b83f17a9508fecd9
dhaferjlassi <dhafer.jlassi@esprit.tn>
2022-02-28 22:53:47 +0100

    controllers

---
+++ src/main/java/tn/esprit/spring/WorkAndWellnessApplication.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAndWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAndWellnessApplication.class, args);
+	}
+
+}
