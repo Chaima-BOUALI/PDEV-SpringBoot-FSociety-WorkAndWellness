36681a032584c68e5cd419f0b610005a7d150f90
chaymaaouadi2704 <chayma.aouadi@esprit.tn>
2022-02-28 22:32:13 +0100

    Quizcontrollers

--- src/main/java/tn/esprit/spring/controllers/AnswerQuizController.java
+++ src/main/java/tn/esprit/spring/controllers/AnswerQuizController.java
@@ -1,9 +1,12 @@
 package tn.esprit.spring.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +20,29 @@ import tn.esprit.spring.services.IServiceAnswerQuiz;
 @RequestMapping("/AnswerQuiz")
 public class AnswerQuizController {
 @Autowired
-IServiceAnswerQuiz answerQuiz; 
+IServiceAnswerQuiz ServiceAnswerQuiz;
+
+@GetMapping("/ShowQuizAnswers/{QuizAnswer-id}")
 @ResponseBody
-@GetMapping("/ShowAnswers")
-public List<AnswerQuiz> getAnswers() {
-List<AnswerQuiz> listAnswers = answerQuiz.retrieveAllAnswers();
-return listAnswers;
+public AnswerQuiz retrieveAnswerQuiz(@PathVariable("QuizAnswer-id") int idAnswerQuiz ) {
+return ServiceAnswerQuiz.retrieveAnswerQuiz(idAnswerQuiz);
+}
+@PostMapping("/add-answers")
+@ResponseBody
+public AnswerQuiz addAnswerQuiz(@RequestBody AnswerQuiz a)
+{
+	AnswerQuiz answers= ServiceAnswerQuiz.addAnswerQuiz(a);
+return answers;
+}
+@DeleteMapping("/remove-answers/{QuizAnswer-id}")
+@ResponseBody
+public void removeAnswers(@PathVariable("QuizAnswer-id") int idAnswerQuiz) {
+ServiceAnswerQuiz.deleteAnswerQuiz(idAnswerQuiz);
+}
+
+@PutMapping("/modify-answers")
+@ResponseBody
+public AnswerQuiz modifyAnswerQuiz(@RequestBody AnswerQuiz answers) {
+return ServiceAnswerQuiz.updateAnswerQuiz(answers);
 }
 }
