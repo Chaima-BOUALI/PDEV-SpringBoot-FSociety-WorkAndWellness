fe93760d9108c9aae562dd5a10e83f3b591f5d8c
Nasri Cyrine <73169571+nasricyrine@users.noreply.github.com>
2022-02-28 18:57:02 +0100

    entities

---
+++ src/main/java/tn/esprit/spring/entities/Subjects.java
@@ -0,0 +1,74 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity(name = "FORUM_SUBJECTS")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EnableScheduling
+@Table(name = "SUBJECT_TABLE")
+public class Subjects implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idSubject; 
+	@Column(length = 3000)
+	private String subjectName; 
+	@Column(length = 3000)
+	private String subjectDescription; 
+	@Enumerated(EnumType.STRING)
+	private Subject Subject; 
+	private Date dateEvent;
+	private int idDocument;
+	@Column(length = 3000)
+	private String documentName;
+	@Column(length = 3000)
+	private String documentType;
+	@Column(length = 3000)
+	@Lob
+	private byte[] dataDocument;
+	private int idImage;
+	@Column(length = 3000)
+	private String imageName;
+	@Column(length = 3000)
+	private String imageType;
+	@Column(length = 3000)
+	private String imageURL;
+	@Lob
+	private byte[] dataImage;
+	@ManyToOne
+	private Forum forum;
+	@ManyToOne
+	private Partnership partnerships;
+}
+
