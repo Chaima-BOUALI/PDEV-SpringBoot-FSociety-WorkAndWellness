07b1c1467169ae3e2a8cf760f1f4f5ad4f5b38db
ChaymaBoualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-03-07 12:33:54 +0100

    Security with UserDetails configuration and IMPL treatment

---
+++ src/main/java/tn/esprit/spring/services/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package tn.esprit.spring.services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import tn.esprit.spring.entities.User;
+
+public class UserDetailsImpl implements UserDetails{
+private User user;
+	
+	public UserDetailsImpl(User user) {
+		
+		super();
+		this.user=user;
+	}
+		
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> grantedAuthorities=new ArrayList<GrantedAuthority>();
+		grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_"+this.user.getRoles()));
+//		System.out.println("inside details impl "+grantedAuthorities.get(0).getAuthority());
+		return grantedAuthorities;	
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getUsername();
+
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
