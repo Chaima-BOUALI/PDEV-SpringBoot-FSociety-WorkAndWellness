73fead72a0476a6daad9087aa05cb45b7491504d
ChaymaBoualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-03-24 00:34:43 +0100

    CustomAuthentificationFilter & attempt & successful authentification

--- src/main/java/tn/esprit/spring/config/SecurityConfig.java
+++ src/main/java/tn/esprit/spring/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package tn.esprit.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import net.bytebuddy.implementation.bind.annotation.Super;
+import tn.esprit.spring.filters.CustomAuthentificationFilter;
 
 @EnableWebSecurity 
 @RequiredArgsConstructor
@@ -24,9 +30,17 @@ auth.userDetailsService(userDetailsService).passwordEncoder(bcryptPasswordEncode
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthentificationFilter(authenticationManagerBean()));
+		
+}
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return  authenticationManagerBean();
+		
 	}
-	
 
 }
